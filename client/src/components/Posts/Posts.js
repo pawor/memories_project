2689a7d68c0cac28dc103018ca2978f7965e35b5
pawor <pawor.emi@gmail.com>
2021-10-16 15:30:27 +0700

    edit project

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <div>
+            <Post /> 
+        </div>
+    );
+}
+
+export default Posts
