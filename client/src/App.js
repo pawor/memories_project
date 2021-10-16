396f75f623d4933ca775f011d18317c58eac6df5
pawor <pawor.emi@gmail.com>
2021-10-16 21:11:33 +0700

    create memories

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { AppBar, Container, Typography, Grow, Grid } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import memories from './images/memories.png';
@@ -7,6 +9,12 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch]);
+
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
